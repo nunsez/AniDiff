1f546dee0e065661053919ae9793e8034b2384c3
Alexander Mandrikov <mandrikov@pm.me>
2023-03-12 10:32:31 +0700

    add json module

---
+++ lib/anidiff/json.ex
@@ -0,0 +1,13 @@
+defmodule Anidiff.Json do
+  @moduledoc false
+
+  @spec parse(String.t()) :: any()
+  def parse(json) do
+    Jason.decode!(json)
+  end
+
+  @spec generate(any()) :: String.t()
+  def generate(term) do
+    Jason.encode!(term)
+  end
+end

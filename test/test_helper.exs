5af89dee2eaae4247b9154d0071c3b15f869f79f
Alexander Mandrikov <mandrikov@pm.me>
2023-03-12 12:38:10 +0700

    add test helper module

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,31 @@
 ExUnit.start()
+
+defmodule Anidiff.TestHelper do
+  @moduledoc false
+
+  alias Anidiff.Html
+
+  @spec fixture(Path.t()) :: String.t()
+  def fixture(path) do
+    path
+    |> Path.expand(fixture_path())
+    |> File.read!()
+  end
+
+  @spec doc(Path.t()) :: Html.document()
+  def doc(file) do
+    doc(file, Html)
+  end
+
+  @spec doc(Path.t(), module()) :: Html.document()
+  def doc(file, parser) do
+    file
+    |> fixture()
+    |> parser.parse()
+  end
+
+  @spec fixture_path() :: Path.t()
+  def fixture_path do
+    Path.join(__DIR__, "fixtures")
+  end
+end

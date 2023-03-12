419c06580fde500cc3c3fec1165f79626d8742e1
Alexander Mandrikov <mandrikov@pm.me>
2023-03-12 10:32:36 +0700

    add http module

---
+++ lib/anidiff/http.ex
@@ -0,0 +1,32 @@
+defmodule Anidiff.Http do
+  @moduledoc false
+
+  alias Anidiff.Json
+
+  @type response() :: map()
+
+  @spec get(String.t()) :: response()
+  def get(url) do
+    headers = [
+      {"content-type", "text/html"}
+    ]
+    request = Finch.build(:get, url, headers)
+
+    {:ok, response} = Finch.request(request, MyFinch)
+
+    response
+  end
+
+  @spec post(String.t(), map()) :: response()
+  def post(url, data) do
+    body = Json.generate(data)
+    headers = [
+      {"content-type", "application/json"}
+    ]
+    request = Finch.build(:post, url, headers, body)
+
+    {:ok, response} = Finch.request(request, MyFinch)
+
+    response
+  end
+end

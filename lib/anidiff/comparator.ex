b75621d8df601b57cd2923a8256480577d6bf3d9
Alexander Mandrikov <mandrikov@pm.me>
2023-03-18 13:16:47 +0700

    add comparator

---
+++ lib/anidiff/comparator.ex
@@ -0,0 +1,79 @@
+defmodule Anidiff.Comparator do
+  @moduledoc false
+
+  alias Anidiff.MangaEntry
+  alias Anidiff.AnimeEntry
+  alias Anidiff.Mal
+  alias Anidiff.Shiki
+
+  def anime do
+    mal_entries =
+      Mal.anime_list()
+      |> to_map_as(AnimeEntry)
+
+    shiki_entries =
+      Shiki.anime_list()
+      |> to_map_as(AnimeEntry)
+
+    mal_diff =
+      Task.async_stream(mal_entries, &compare(&1, shiki_entries))
+      |> to_list()
+
+    shiki_diff =
+      Task.async_stream(shiki_entries, &compare(&1, mal_entries))
+      |> to_list()
+
+    Enum.uniq_by(mal_diff ++ shiki_diff, fn e -> e.id end)
+  end
+
+  def manga do
+    mal_entries =
+      Mal.manga_list()
+      |> to_map_as(MangaEntry)
+
+    shiki_entries =
+      Shiki.manga_list()
+      |> to_map_as(MangaEntry)
+
+    mal_diff =
+      Task.async_stream(mal_entries, &compare(&1, shiki_entries))
+      |> to_list()
+
+    shiki_diff =
+      Task.async_stream(shiki_entries, &compare(&1, mal_entries))
+      |> to_list()
+
+    Enum.uniq_by(mal_diff ++ shiki_diff, fn e -> e.id end)
+  end
+
+  def compare({anime_id, entry}, other_entries) when is_struct(entry, AnimeEntry) do
+    equal_result =
+      other_entries
+      |> Map.get(anime_id)
+      |> AnimeEntry.equals?(entry)
+
+    if equal_result, do: nil, else: entry
+  end
+
+  def compare({manga_id, entry}, other_entries) when is_struct(entry, MangaEntry) do
+    equal_result =
+      other_entries
+      |> Map.get(manga_id)
+      |> MangaEntry.equals?(entry)
+
+    if equal_result, do: nil, else: entry
+  end
+
+  def to_list(stream) do
+    stream
+    |> Stream.map(fn {:ok, term} -> term end)
+    |> Stream.reject(&is_nil/1)
+    |> Enum.to_list()
+  end
+
+  def to_map_as(string, struct) do
+    string
+    |> Enum.map(&struct.new/1)
+    |> Enum.into(%{}, fn e -> {e.id, e} end)
+  end
+end

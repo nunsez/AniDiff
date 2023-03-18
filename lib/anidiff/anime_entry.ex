280992b46b0238ce10ddcf38ca2faacde20221ee
Alexander Mandrikov <mandrikov@pm.me>
2023-03-18 13:16:38 +0700

    add entries

---
+++ lib/anidiff/anime_entry.ex
@@ -0,0 +1,65 @@
+defmodule Anidiff.AnimeEntry do
+  @moduledoc false
+
+  defstruct [
+    :id,
+    :title,
+    :status,
+    :score,
+    :episodes_watched,
+    :source
+  ]
+
+  # Mal constructor
+  def new(%{
+        "anime_id" => id,
+        "anime_title" => title,
+        "status" => status,
+        "score" => score,
+        "num_watched_episodes" => episodes_watched
+      })
+      when not is_nil(id) do
+    %__MODULE__{
+      id: id,
+      title: title,
+      status: format_status(status),
+      score: score,
+      episodes_watched: episodes_watched,
+      source: :mal
+    }
+  end
+
+  # Shiki constructor
+  def new(%{
+        "target_id" => id,
+        "target_title" => title,
+        "status" => status,
+        "score" => score,
+        "episodes" => episodes_watched
+      })
+      when not is_nil(id) do
+    %__MODULE__{
+      id: id,
+      title: title,
+      status: status,
+      score: score,
+      episodes_watched: episodes_watched,
+      source: :shiki
+    }
+  end
+
+  def equals?(a, b) when is_struct(a, __MODULE__) and is_struct(b, __MODULE__) do
+    a.status == b.status and
+      a.score == b.score and
+      a.episodes_watched == b.episodes_watched
+  end
+
+  def equals?(_, _), do: false
+
+  defp format_status(1), do: "watching"
+  defp format_status(2), do: "completed"
+  defp format_status(3), do: "on_hold"
+  defp format_status(4), do: "dropped"
+  defp format_status(6), do: "planned"
+  defp format_status(x), do: to_string(x)
+end
---
+++ lib/anidiff/manga_entry.ex
@@ -0,0 +1,70 @@
+defmodule Anidiff.MangaEntry do
+  @moduledoc false
+
+  defstruct [
+    :id,
+    :title,
+    :status,
+    :score,
+    :chapters_read,
+    :volumes_read,
+    :source
+  ]
+
+  # Mal constructor
+  def new(%{
+        "manga_id" => id,
+        "manga_title" => title,
+        "status" => status,
+        "score" => score,
+        "num_read_chapters" => chapters_read,
+        "manga_num_volumes" => volumes_read
+      })
+      when not is_nil(id) do
+    %__MODULE__{
+      id: id,
+      title: title,
+      status: format_status(status),
+      score: score,
+      chapters_read: chapters_read,
+      volumes_read: volumes_read,
+      source: :mal
+    }
+  end
+
+  # Shiki constructor
+  def new(%{
+        "target_id" => id,
+        "target_title" => title,
+        "status" => status,
+        "score" => score,
+        "chapters" => chapters_read,
+        "volumes" => volumes_read
+      })
+      when not is_nil(id) do
+    %__MODULE__{
+      id: id,
+      title: title,
+      status: status,
+      score: score,
+      chapters_read: chapters_read,
+      volumes_read: volumes_read,
+      source: :shiki
+    }
+  end
+
+  def equals?(a, b) when is_struct(a, __MODULE__) and is_struct(b, __MODULE__) do
+    (a.chapters_read == b.chapters_read or a.volumes_read == b.volumes_read) and
+      a.status == b.status and
+      a.score == b.score
+  end
+
+  def equals?(_, _), do: false
+
+  defp format_status(1), do: "watching"
+  defp format_status(2), do: "completed"
+  defp format_status(3), do: "on_hold"
+  defp format_status(4), do: "dropped"
+  defp format_status(6), do: "planned"
+  defp format_status(x), do: to_string(x)
+end

e4e4259767bfc2c9229a3435864ec4034ebe2a93
Alexander Mandrikov <mandrikov@pm.me>
2023-03-18 09:32:16 +0700

    update shiki module

--- lib/anidiff/shiki.ex
+++ lib/anidiff/shiki.ex
@@ -1,11 +1,26 @@
 defmodule Anidiff.Shiki do
   @moduledoc false
 
+  alias Anidiff.Env
+  alias Anidiff.Http
+  alias Anidiff.Json
+
   def anime_list do
-    []
+    url = "https://shikimori.one/#{Env.shiki_username()}/list_export/animes.json"
+
+    fetch(url, Http)
   end
 
   def manga_list do
-    []
+    url = "https://shikimori.one/#{Env.shiki_username()}/list_export/mangas.json"
+
+    fetch(url, Http)
+  end
+
+  def fetch(url, http_client) do
+    url
+    |> http_client.get()
+    |> then(&(&1.body))
+    |> Json.parse()
   end
 end
